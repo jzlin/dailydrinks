00ed61636a8f5f3d1f1d7d6bf116d3902b0eef06
Jerry Lin <jerry.jzlin@gmail.com>
2019-06-05 22:48:19 +0800

    Implement UI for orders list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  orders: any[] = [
+    {
+      name: '紅茶',
+      price: 35,
+      notes: '無糖去冰',
+    },
+    {
+      name: '綠茶',
+      price: 35,
+      notes: null,
+    },
+    {
+      name: '珍珠奶茶',
+      price: 1000,
+      notes: `半糖微冰
+      特大杯`,
+    },
+    {
+      name: '烏龍茶',
+      price: 50,
+      notes: `微糖去冰
+      +珍珠`,
+    },
+  ];
 }
